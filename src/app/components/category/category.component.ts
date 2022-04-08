f98e67e052e40fd4d6781f7099ea5d012634850a
halilkaragz <halilkaragoz_blsm@hotmail.com>
2022-04-08 17:30:35 +0300

    8 nisan commit 3

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,38 +5,41 @@ import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
-  
-  currentCategory :Category;
-  categories : Category[] = [];
-  constructor(private categoryService: CategoryService) { }
+  currentCategory: Category;
+  categories: Category[] = [];
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
     this.getCategories();
   }
 
-  getCategories(){
-    this.categoryService.getCategories().subscribe(response=>{
-        this.categories=response.data;        
-      });
+  getCategories() {
+    this.categoryService.getCategories().subscribe((response) => {
+      this.categories = response.data;
+    });
   }
 
-  setCurrentCategory(category:Category){
+  setCurrentCategory(category: Category) {
     this.currentCategory = category;
   }
 
-  getCurrentCategoryClass(category:Category){
-    if(category == this.currentCategory)
-    {
-      return "list-group-item active"
+  getCurrentCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item active'
+    } else {
+      return 'list-group-item'
     }
-    else
-    {
-      return "list-group-item"
+  }
+
+  getAllCategoryClass() {
+    if (!this.currentCategory) {
+      return 'list-group-item active'
+    } else {
+      return 'list-group-item'
     }
-      
   }
   
 }
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/moduls/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -13,16 +14,30 @@ export class ProductComponent implements OnInit {
   products: Product[] = [];
   dataLoaded = false;
   
-
-  constructor(private productService:ProductService) {}
+// ActivatedRoute Angular da BuildIn bir servisi, aktifleştirilmiş route yani mevcut route
+  constructor(private productService:ProductService, 
+    private activatedRoute:ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.getProducts();
+    this.activatedRoute.params.subscribe(params=>{
+      if (params["categoryId"])
+      {
+        this.getProductsByCategory(params["categoryId"])
+      }
+      else{this.getProducts()}
+    })
    
   }
 
   getProducts(){
     this.productService.getProducts().subscribe(response=>{
+        this.products=response.data; 
+        this.dataLoaded = true
+      });
+  }
+
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsbyCategory(categoryId).subscribe(response=>{
         this.products=response.data; 
         this.dataLoaded = true
       });
